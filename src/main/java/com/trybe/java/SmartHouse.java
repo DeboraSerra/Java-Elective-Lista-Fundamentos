0b9b9c318add9be6b1204534eb0c683db31ebc0d
Débora Serra <debora.r.serra@gmail.com>
2024-05-18 16:47:17 -0300

    feat: add requirement one

--- src/main/java/com/trybe/java/SmartHouse.java
+++ src/main/java/com/trybe/java/SmartHouse.java
@@ -4,5 +4,21 @@ package com.trybe.java;
  * Classe.
  **/
 public class SmartHouse {
+    private boolean isLampadaLigada = false;
 
+    public SmartHouse() {
+        this.isLampadaLigada = true;
+    }
+
+    void ligarLampada() {
+        this.isLampadaLigada = true;
+    }
+
+    void desligarLampada() {
+        this.isLampadaLigada = false;
+    }
+
+    boolean isLampadaLigada() {
+        return  this.isLampadaLigada;
+    }
 }
