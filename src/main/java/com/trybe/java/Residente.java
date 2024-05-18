962d93af30ff0aec57403e28ecbf02d38322fdba
Débora Serra <debora.r.serra@gmail.com>
2024-05-18 16:52:49 -0300

    feat: add requirement two

--- src/main/java/com/trybe/java/Residente.java
+++ src/main/java/com/trybe/java/Residente.java
@@ -4,5 +4,15 @@ package com.trybe.java;
  * Classe.
  **/
 public class Residente {
+    public String nome;
+    public int idade;
+    public double peso;
+    public  double altura;
 
+    public  Residente(String nome, int idade, double peso, double altura) {
+        this.altura = altura;
+        this.idade = idade;
+        this.nome = nome;
+        this.peso = peso;
+    }
 }
